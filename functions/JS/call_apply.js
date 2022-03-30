4f2b4be4e6d477a4c1f4bfa5639996e583f3a928
Semandr <semandr.code@gmail.com>
2022-03-30 08:48:24 +0200

    study of .bind() method

--- functions/JS/call_apply.js
+++ functions/JS/call_apply.js
@@ -43,3 +43,41 @@ const flightData = [583, 'George Cooper'];
 book.apply(swiss, flightData);
 console.log(swiss);
 book.call(swiss, ...flightData);
+
+// BIND method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(23, 'Stewen Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Greet');
+bookEW23('Pinco di Pallino');
+
+// Winth EventListeners .bind()
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addIVA = addTax.bind(null, 0.23);
+// addIVA = value => value + value * 0.23
+console.log(addIVA(100)); // 123
+
+// Rewrite addTax function
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addIVA2 = addTaxRate(0.23);
+console.log(addIVA2(1000));
