2fbd660d72881fa58b57047dd6c0357a3496d5d2
Semandr <semandr.code@gmail.com>
2022-04-01 19:48:46 +0200

    Study how .forEach() method work not only with arrays, but also with MAP & SET

--- arrays/js/script.js
+++ arrays/js/script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -164,3 +164,28 @@ movements.forEach((movement, index, array) => {
 
 
 */
+
+/////////////////////////////////////////////////
+// forEach() METHOD with MAPS
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach((value, key, map) => {
+  console.log(`${key}: ${value}. And ${map}`);
+});
+
+// Output like: USD: United States dollar. And [object Map]
+
+/////////////////////////////////////////////////
+// forEach() METHOD with SET
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach((value, _, map) => {
+  console.log(`${value}: ${value}. And ${map}`);
+});
+// _ === value that completely Unnecessary
+// Output like: USD: USD. And [object Set]
