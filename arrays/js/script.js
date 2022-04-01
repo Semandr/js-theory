3fe5eff7622ed2e9e6e40a6aa3963682e581eff0
Semandr <semandr.code@gmail.com>
2022-04-01 20:13:15 +0200

    Write function that working with .forEach() method to add a new htlm-element to the DOM

--- arrays/js/script.js
+++ arrays/js/script.js
@@ -61,15 +61,32 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+function displayMovements(movements) {
+  movements.forEach((el, i) => {
+    const type = el > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${el}</div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+}
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -163,16 +180,10 @@ movements.forEach((movement, index, array) => {
 });
 
 
-*/
+
 
 /////////////////////////////////////////////////
 // forEach() METHOD with MAPS
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
 
 currencies.forEach((value, key, map) => {
   console.log(`${key}: ${value}. And ${map}`);
@@ -189,3 +200,6 @@ currenciesUnique.forEach((value, _, map) => {
 });
 // _ === value that completely Unnecessary
 // Output like: USD: USD. And [object Set]
+
+
+*/
