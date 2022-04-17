cb41ee0b5920a2bdf1fed15bd2dfb008b182135d
Semandr <semandr.code@gmail.com>
2022-04-17 14:17:38 +0200

    Constructor functions and the ~new~ Operator

--- OOP/script.js
+++ OOP/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const marco = new Person('Marco', 1991);
+console.log(marco);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(matilda, jack);
+
+console.log(marco instanceof Person); // true
