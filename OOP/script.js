c8871dbec8926afb84ac801dffdf35c72f334e70
Semandr <semandr.code@gmail.com>
2022-04-17 16:25:12 +0200

    Prototype and Prototype chain

--- OOP/script.js
+++ OOP/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+///////////////////////////////////////////////////
+// FUNCTION CONSTRUCTOR
+///////////////////////
+//
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -14,3 +18,21 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(marco instanceof Person); // true
+
+///////////////////////////////////////////////////
+// PROTOTYPES
+///////////////////////
+//
+Person.prototype.calcAge = function () {
+  console.log(2022 - this.birthYear);
+};
+console.log(Person.prototype);
+
+marco.calcAge();
+console.log(marco.__proto__);
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(marco.species, matilda.species); // Homo Sapiens
+
+console.log(matilda.hasOwnProperty('firstName')); // true
+console.log(matilda.hasOwnProperty('species')); // false
