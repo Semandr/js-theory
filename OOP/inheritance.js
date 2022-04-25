d0878dce3470f2745caa7cc857eebc48a2101ab2
Semandr <semandr.code@gmail.com>
2022-04-25 22:04:54 +0200

    complete another OOP challenge with Prototype inheritance between constructor functions

--- OOP/inheritance.js
+++ OOP/inheritance.js
@@ -4,7 +4,7 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 };
 Person.prototype.calcAge = function () {
-  console.log(2022 - this.birthYear);
+  console.log(2037 - this.birthYear);
 };
 
 const Student = function (firstName, birthYear, course) {
@@ -12,22 +12,15 @@ const Student = function (firstName, birthYear, course) {
   this.course = course;
 };
 
-// Linking prototypes
+// Connecting Prototypes
 Student.prototype = Object.create(Person.prototype);
 
 Student.prototype.introduce = function () {
   console.log(`My name is ${this.firstName} and I study ${this.course}`);
 };
 
-const mike = new Student('Mike', 2000, 'Computer Science');
-mike.introduce();
-mike.calcAge(); // 22
+Student.prototype.constructor = Student;
 
-console.log(mike.__proto__);
-console.log(mike.__proto__.__proto__);
-console.log(mike instanceof Student);
-console.log(mike instanceof Person);
-console.log(mike instanceof Object);
-
-Student.prototype.costructor = Student;
-console.dir(Student.prototype.constructor);
+const mike = new Student('Mike', 2020, 'Computer Sciensce');
+mike.introduce(); // My name is Mike and I study Computer Sciensce
+mike.calcAge(); // 17
