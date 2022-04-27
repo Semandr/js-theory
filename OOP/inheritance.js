f2a1bd978f949cb28d9330d208db357d8dce3095
Semandr <semandr.code@gmail.com>
2022-04-27 12:06:40 +0200

    another ES6 Class methods

--- OOP/inheritance.js
+++ OOP/inheritance.js
@@ -85,7 +85,7 @@ martha.introduce();
 martha.calcAge();
 
 
-*/
+
 
 /////////////////////////////////////////////////
 // INHERITANCE BETWEEN CLASSES OBJECT.CREATE()
@@ -121,3 +121,52 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Biology');
 jay.introduce(); // My name is Jay and I study Biology
 jay.calcAge(); // 27
+
+
+
+*/
+
+/////////////////////////////////////////////////
+// ANOTHER CLASS EXAMPLE
+////////////////////////////////////////////////
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account ${owner}`);
+  }
+
+  // Public interface of our object
+  deposit(value) {
+    this.movements.push(value);
+  }
+  withdraw(value) {
+    this.deposit(-value);
+  }
+  approveLoan(value) {
+    return true;
+  }
+  requestLoan(value) {
+    if (this.approveLoan(value)) {
+      this.deposit(value);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+
+console.log(acc1);
