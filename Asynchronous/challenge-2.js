04c2ed821d1b7dce81c20a794b8d12ce332c03b3
Semandr <semandr.code@gmail.com>
2022-05-11 15:46:22 +0200

    second challenge complete

--- Asynchronous/challenge-2.js
+++ Asynchronous/challenge-2.js
@@ -1,7 +1,41 @@
 'use strict';
 
-function createImage(imgPath) {
-  return new Promise(function (resolve, reject) {
-    resolve() {document.createElement('img')}
+const imgBox = document.querySelector('.images');
+let currentImg;
+function wait(seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
   });
 }
+function createImage(imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', () => {
+      imgBox.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', () => {
+      reject(new Error(`Image not found`));
+    });
+  });
+}
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.log(err));
